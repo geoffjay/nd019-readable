c9e728300cef309a9eef2268636b0d9df4660925
Geoff Johnson <geoff.jay@gmail.com>
2018-03-11 16:27:35 -0700

    feat: Add API functions

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,24 @@
+const host = process.env.READABLE_HOST || 'localhost'
+const port = process.env.READABLE_PORT || 3001
+
+// Generate a unique token for the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+const api = `http://${host}:${port}`
+
+export const getCategories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+export const getCategoryPosts = (categoryId) =>
+  fetch(`${api}/${categoryId}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data.posts)
