56efaa147f2112381567dd3a99ae5d85c8eaf48e
Geoff Johnson <geoff.jay@gmail.com>
2018-04-14 11:21:23 -0700

    refactor: Clean up action types

---
+++ frontend/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const initialStore = {
+  categories: {},
+  posts: [],
+  comments: [],
+}
+
+const store = createStore(
+  reducer,
+  initialStore,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
+
+export default store
