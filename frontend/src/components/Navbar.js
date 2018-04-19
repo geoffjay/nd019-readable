639d898dd1e10e28cb88b03c1cfe4f8c3f079135
Geoff Johnson <geoff.johnson@coanda.ca>
2018-04-19 10:34:58 -0700

    feat: Add navigation functionality to app bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
@@ -8,6 +8,7 @@ import IconButton from 'material-ui/IconButton'
 import Button from 'material-ui/Button'
 import MenuIcon from 'material-ui-icons/Menu'
 import ExpandIcon from 'material-ui-icons/ExpandMore'
+import Menu, { MenuItem } from 'material-ui/Menu'
 
 const styles = {
   root: {
@@ -25,39 +26,79 @@ const styles = {
 const propTypes = {
   classes: PropTypes.object.isRequired,
   toggleSidebar: PropTypes.func.isRequired,
+  selectSort: PropTypes.func.isRequired,
 }
 
-const Navbar = (props) => {
+class Navbar extends Component {
 
-  const { classes, toggleSidebar } = props
+  state = {
+    anchorEl: null,
+  }
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="Menu"
-            onClick={toggleSidebar(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="title"
-            color="inherit"
-            className={classes.flex}
-          >
-            Readable
-          </Typography>
-          <Button color="inherit">
-            Sort by
-            <ExpandIcon />
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  )
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget })
+  }
+
+  handleClose = () => {
+    this.setState({ anchorEl: null })
+  }
+
+  handleSelect = (sortBy) => {
+    const { selectSort } = this.props
+    selectSort(sortBy)
+    this.handleClose()
+  }
+
+  render() {
+    const { classes, toggleSidebar } = this.props
+    const { anchorEl } = this.state
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              onClick={toggleSidebar(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
+              Readable
+            </Typography>
+            <Button
+              color="inherit"
+              aria-owns={anchorEl ? 'sort-menu' : null}
+              aria-haspopup="true"
+              onClick={this.handleClick}
+            >
+              Sort by
+              <ExpandIcon />
+            </Button>
+            <Menu
+              id="sort-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={() => this.handleSelect('popularity')}>
+                Popularity
+              </MenuItem>
+              <MenuItem onClick={() => this.handleSelect('date')}>
+                Date
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
 }
 
 Navbar.propTypes = propTypes
